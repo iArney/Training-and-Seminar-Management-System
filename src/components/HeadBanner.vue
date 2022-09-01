6b008b5e79f49874cfdeefd029cc3621d12a977a
eric lyimo <riclyimo29@gmail.com>
2022-09-01 14:02:33 +0300

    add circle-decoration

--- src/components/HeadBanner.vue
+++ src/components/HeadBanner.vue
@@ -1,26 +1,39 @@
 <template>
-  <v-container id="Banner" class="d-flex justify-between fs-6" fluid>
+  <v-container id="Banner" fluid>
     <v-row no-gutters>
       <v-col cols="6" md="7" sm="8" xs="12" class="pt-16">
-        <div class="mt-10 mx-14">
-          <h1 class="lh-base xl-text">
-            Training And Seminars Management
-            <span class="deep--text">System</span>
-          </h1>
-          <p class="lh-lg text">
-            The Training and Seminar Management System (TSMS) has been developed
-            to maintain records, provide training information and monitor the
-            information of trainers and instructors regarding training,
-            seminars, workshops, annual meetings and sessions of systems and
-            services of the Internet Government Authority
-          </p>
+        <v-container fluid>
+          <v-row no-gutters>
+            <v-col lg="11" sm="12">
+              <div class="mt-10 me-5">
+                <h1 class="lh-base xl-text">
+                  Training And Seminars Management
+                  <span class="deep--text">System</span>
+                </h1>
+                <p class="lh-lg text">
+                  The Training and Seminar Management System (TSMS) has been
+                  developed to maintain records, provide training information
+                  and monitor the information of trainers and instructors
+                  regarding training, seminars, workshops, annual meetings and
+                  sessions of systems and services of the Internet Government
+                  Authority
+                </p>
 
-          <div id="triangle-decorator" class="mt-12 deep"></div>
-          <div class="mt-5 ms-16 ps-16">
-            <button class="btn btn-lg deep  white--text border rounded-pill py-3 px-10">Read More...</button>
-          </div>
-          
-        </div>
+                <div id="triangle-decorator" class="mt-12 deep"></div>
+                <div class="mt-5 ms-16 ps-16">
+                  <button
+                    class="btn btn-lg deep white--text border rounded-pill py-3 px-10"
+                  >
+                    Read More...
+                  </button>
+                </div>
+              </div>
+            </v-col>
+            <v-col cols="1" class="d-lg-flex align-center d-none">
+              <div id="circle-decoration" class="rounded-circle"></div>
+            </v-col>
+          </v-row>
+        </v-container>
       </v-col>
       <v-col class="overflow-hidden" id="right" cols="6" md="5" sm="4">
         <v-container>
@@ -33,10 +46,7 @@
                 srcset=""
               />
             </v-col>
-            <v-col
-              cols="1"
-              class="d-flex align-center ps-10 pt-16 mt-16 pb-5"
-            >
+            <v-col cols="1" class="d-flex align-center ps-10 pt-16 mt-16 pb-5">
               <font-awesome-icon
                 class="deep--text float-right"
                 icon="fa-solid fa-star fs-"
@@ -68,6 +78,11 @@ export default {};
     #d5e6fb 97.83%
   );
 }
+#circle-decoration {
+  width: 67px;
+  height: 67px;
+  border: 7px solid #004b8e;
+}
 .nav {
   width: 0 !important;
 }
@@ -81,7 +96,7 @@ export default {};
 .text {
   font-size: 1.125rem;
 }
-#triangle-decorator{
+#triangle-decorator {
   transform: matrix(0.59, -0.81, 0.81, 0.59, 0, 0);
   width: 50.02px;
   height: 53.1px;
@@ -95,7 +110,7 @@ export default {};
 
 @media only screen and (max-width: 1144px) {
   .xl-text {
-    font-size: 2.8rem !important;
+    font-size: 2.6rem !important;
   }
 }
 
